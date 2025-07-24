830d4c94bcafd800149cb8d3eccec540d53de3f8
marykasp <kasparian.mary@gmail.com>
2025-07-24 08:45:21 -0500

    Create and style the 4 stat containers to display words, characters

--- word-analytics/src/components/Stats.jsx
+++ word-analytics/src/components/Stats.jsx
@@ -1,5 +1,24 @@
 const Stats = () => {
-  return <section className="stats">Stats</section>;
+  return (
+    <section className="stats">
+      <section className="stat">
+        <span className="stat__number">0</span>
+        <h2 className="second-heading">Words</h2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">0</span>
+        <h2 className="second-heading">Characters</h2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">280</span>
+        <h2 className="second-heading">Instagram</h2>
+      </section>
+      <section className="stat">
+        <span className="stat__number">2200</span>
+        <h2 className="second-heading">Facebook</h2>
+      </section>
+    </section>
+  );
 };
 
 export default Stats;
