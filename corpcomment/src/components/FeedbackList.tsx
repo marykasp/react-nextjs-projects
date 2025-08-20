e26c7d3295dff7b4981e5e60368a04cc27cc838b
marykasp <kasparian.mary@gmail.com>
2025-08-20 12:27:46 -0500

    Add in loading and error message state to display

--- corpcomment/src/components/FeedbackList.tsx
+++ corpcomment/src/components/FeedbackList.tsx
@@ -1,25 +1,43 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
+import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 const FeedbackList = () => {
   const [feedbackItems, setFeedbackItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
+    setIsLoading(true);
+
     // fetch data from API server - interact with system outside of React
     fetch(
       "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
     )
       .then((response) => {
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+
         return response.json();
       })
       .then((data) => {
         // console.log(data.feedbacks);
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        setErrorMessage("Something went wrong");
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <ol className="feedback-list">
+      {isLoading && <Spinner />}
+
+      {errorMessage && <ErrorMessage message={errorMessage} />}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem feedbackItem={feedbackItem} key={feedbackItem.id} />
       ))}
