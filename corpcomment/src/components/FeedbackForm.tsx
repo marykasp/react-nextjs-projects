1b0ef7798c5ab2bbdd65a09e924a965401d593de
marykasp <kasparian.mary@gmail.com>
2025-08-20 10:00:35 -0500

    Create controlled input for feedback form, show characters left

--- corpcomment/src/components/FeedbackForm.tsx
+++ corpcomment/src/components/FeedbackForm.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react";
+import { MAX_CHARACTERS } from "../lib/constants";
+
 const FeedbackForm = () => {
+  const [text, setText] = useState("");
+
+  // get number of characters left if limit is 150
+  const charCount = MAX_CHARACTERS - text.length;
+
   return (
     <form className="form">
-      <label htmlFor="feedback-textarea">
-        Enter your feedback here, remember to #hashtag the company
-      </label>
       <textarea
-        name=""
+        value={text}
         id="feedback-textarea"
         placeholder="feedback"
         spellCheck={false}
+        onChange={(e) => setText(e.target.value)}
+        maxLength={MAX_CHARACTERS}
       />
+      <label htmlFor="feedback-textarea">
+        Enter your feedback here, remember to #hashtag the company
+      </label>
 
       <div>
-        <p className="u-italic">150</p>
+        <p className="u-italic">{charCount}</p>
         <button type="submit">
           <span>Submit</span>
         </button>
