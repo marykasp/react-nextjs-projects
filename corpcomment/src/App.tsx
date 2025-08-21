f78fa545e163139d6b328a0f4f56d94fd43d617e
marykasp <kasparian.mary@gmail.com>
2025-08-21 11:59:43 -0500

    Create FeedbackItemsContextProvider component to hold state and handler functions that are currently on parent App

--- corpcomment/src/App.tsx
+++ corpcomment/src/App.tsx
@@ -1,13 +1,10 @@
 import Footer from "./components/layout/Footer";
 import Container from "./components/layout/Container";
 import HashtagList from "./components/hashtag/HashtagList";
-import { useEffect, useMemo, useState } from "react";
-import type { TFeedbackItem } from "./lib/type";
+import { useMemo, useState } from "react";
+import FeedbackItemsContextProvider from "./components/context/FeedbackItemsContextProvider";
 
 function App() {
-  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-  const [errorMessage, setErrorMessage] = useState<string>("");
   const [selectedCompany, setSelectedCompany] = useState("");
 
   // only filter feedbackItems if there is a selectedCompany
@@ -21,78 +18,9 @@ function App() {
     [feedbackItems, selectedCompany],
   );
 
-  const companyList = useMemo(
-    () =>
-      feedbackItems
-        .map((item) => item.company)
-        .filter((company, index, array) => {
-          return array.indexOf(company) === index;
-        }),
-    [feedbackItems],
-  );
-
   const handleSelectCompany = (company: string) => {
     setSelectedCompany(company);
   };
-
-  const handleAddToList = async (text: string) => {
-    // validation of user input will occur before being passed to this f(x) to add object to the list
-    const companyName = text
-      .split(" ")
-      .find((word: string) => word.includes("#"))!
-      .substring(1);
-
-    const newItem: TFeedbackItem = {
-      id: new Date().getTime(),
-      text: text,
-      upvoteCount: 0,
-      daysAgo: 0,
-      company: companyName,
-      badgeLetter: companyName?.substring(0, 1).toUpperCase(),
-    };
-
-    //add new item to end of list
-    setFeedbackItems([...feedbackItems, newItem]);
-
-    // submit new item to the backend server
-    await fetch(
-      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
-      {
-        method: "POST",
-        body: JSON.stringify(newItem),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-        },
-      },
-    );
-  };
-
-  useEffect(() => {
-    const fetchFeedbackItems = async () => {
-      setIsLoading(true);
-
-      try {
-        const response = await fetch(
-          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
-        );
-
-        if (!response.ok) {
-          throw new Error("Something went wrong");
-        }
-
-        const data = await response.json();
-        setFeedbackItems(data.feedbacks);
-      } catch (error) {
-        console.log(error);
-        setErrorMessage("Something went wrong. Please try again");
-      }
-
-      setIsLoading(false);
-    };
-
-    fetchFeedbackItems();
-  }, []);
 
   return (
     <div className="app">
---
+++ corpcomment/src/components/context/FeedbackItemsContextProvider.tsx
@@ -0,0 +1,102 @@
+import { createContext, useEffect, useMemo, useState } from "react";
+import type { TFeedbackItem } from "../../lib/type";
+
+type TFeedbackItemsContext = {
+  feedbackItems: TFeedbackItem[];
+  isLoading: boolean;
+  errorMessage: string;
+  companyList: string[];
+  handleAddToList: (text: string) => void;
+};
+
+// union type since context will be null inside the Provider component
+const FeedbackItemsContext = createContext<TFeedbackItemsContext | null>(null);
+
+const FeedbackItemsContextProvider = () => {
+  const [feedbackItems, setFeedbackItems] = useState<TFeedbackItem[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  const companyList = useMemo(
+    () =>
+      feedbackItems
+        .map((item) => item.company)
+        .filter((company, index, array) => {
+          return array.indexOf(company) === index;
+        }),
+    [feedbackItems],
+  );
+
+  const handleAddToList = async (text: string) => {
+    // validation of user input will occur before being passed to this f(x) to add object to the list
+    const companyName = text
+      .split(" ")
+      .find((word: string) => word.includes("#"))!
+      .substring(1);
+
+    const newItem: TFeedbackItem = {
+      id: new Date().getTime(),
+      text: text,
+      upvoteCount: 0,
+      daysAgo: 0,
+      company: companyName,
+      badgeLetter: companyName?.substring(0, 1).toUpperCase(),
+    };
+
+    //add new item to end of list
+    setFeedbackItems([...feedbackItems, newItem]);
+
+    // submit new item to the backend server
+    await fetch(
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+      {
+        method: "POST",
+        body: JSON.stringify(newItem),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+      },
+    );
+  };
+
+  useEffect(() => {
+    const fetchFeedbackItems = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(
+          "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks",
+        );
+
+        if (!response.ok) {
+          throw new Error("Something went wrong");
+        }
+
+        const data = await response.json();
+        setFeedbackItems(data.feedbacks);
+      } catch (error) {
+        console.log(error);
+        setErrorMessage("Something went wrong. Please try again");
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchFeedbackItems();
+  }, []);
+
+  return (
+    <FeedbackItemsContext.Provider
+      value={{
+        feedbackItems,
+        isLoading,
+        errorMessage,
+        companyList,
+        handleAddToList,
+      }}
+    ></FeedbackItemsContext.Provider>
+  );
+};
+
+export default FeedbackItemsContextProvider;
