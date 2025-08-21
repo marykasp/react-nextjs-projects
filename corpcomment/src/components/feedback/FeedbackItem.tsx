30a4bbb1fb7c1cc78d64ea671e82c3c82e2aeed5
marykasp <kasparian.mary@gmail.com>
2025-08-21 11:12:58 -0500

    Toggle open state for FeedbackItem component

--- corpcomment/src/components/feedback/FeedbackItem.tsx
+++ corpcomment/src/components/feedback/FeedbackItem.tsx
@@ -1,12 +1,18 @@
 import { TriangleUpIcon } from "@radix-ui/react-icons";
 import { type TFeedbackItem } from "../../lib/type";
+import { useState } from "react";
 
 // for props object
 type FeedbackItemProps = { feedbackItem: TFeedbackItem };
 
 const FeedbackItem = ({ feedbackItem }: FeedbackItemProps) => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <li className="feedback">
+    <li
+      className={`feedback ${open ? "feedback--expand" : ""}`}
+      onClick={() => setOpen((prev) => !prev)}
+    >
       <button>
         <TriangleUpIcon />
         <span>{feedbackItem.upvoteCount}</span>
